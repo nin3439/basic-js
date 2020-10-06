2626ed7767b47d9c7306d11ac8c89c8838aacf71
nin3439 <ninok33_94@mail.ru>
2020-10-06 20:12:32 +0300

    feat: implement fn countCats

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let newArr = arr.filter(item => (typeof item === "string"));
+  return newArr.map(item => item[0]).sort().join(''); 
 };
