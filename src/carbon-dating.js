99d69da6b078c21528517b50c3e28129758013b3
nin3439 <ninok33_94@mail.ru>
2020-10-10 20:35:47 +0300

    feat: finish to implement carbon and dream functions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,16 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity !== 'string') return false; 
+  let newSampleActivity = Number(sampleActivity);
+  if (
+    typeof sampleActivity !== 'string' || 
+    Number.isNaN(newSampleActivity) ||
+    !sampleActivity.trim() ||
+    newSampleActivity >= 15 ||
+    newSampleActivity <= 0
+  ) return false;
 
   const k = 0.693 / HALF_LIFE_PERIOD;
-  let t = (Math.log(MODERN_ACTIVITY / (sampleActivity*1))) / k;
+  let t = (Math.log(MODERN_ACTIVITY / (newSampleActivity))) / k;
   return Math.ceil(t);
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -4,6 +4,6 @@ module.exports = function createDreamTeam(array) {
   if (typeof array === null || typeof array === "undefined" || !Array.isArray(array)) return false;
 
   let newArr = array.filter(item => (typeof item === "string"));
-  let stringNames = newArr.map(item => item.replace(/\s/g, ''));
-  return stringNames.map(item => item[0]).sort().join('').toUpperCase(); 
+  let stringNames = newArr.map(item => item.trim());
+  return stringNames.map(item => item[0].toUpperCase()).sort().join(''); 
 };
