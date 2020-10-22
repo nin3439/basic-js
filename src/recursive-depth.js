d6e3652e40716a852e27cf12c3bb252e1b5d9fff
nin3439 <ninok33_94@mail.ru>
2020-10-22 21:17:58 +0300

    feat: complete task recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,5 +2,19 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
+    let level = 1;
+
+    let locLevel = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+
+        let depth = this.calculateDepth(arr[i]);
+        if (locLevel < depth) {
+          locLevel = depth;
+        }
+      }
+    }
+    level += depth;
+    return level;
   }
 };
