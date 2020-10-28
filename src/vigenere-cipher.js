00fd4a5e6af18808a50bc02fa69758e08a7a7e4b
nin3439 <ninok33_94@mail.ru>
2020-10-28 18:11:03 +0300

    feat: almost complete vigenere task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,11 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 class VigenereCipheringMachine {
+
+  constructor(type) {
+    this.type = type ? 'direct' : 'reverse';
+  }
+
   encrypt(message, key) {
-    if (!message, !key) throw "Error"
-  }    
+
+    if (message === undefined || key === undefined) throw new "Error"();
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let passwordString = "";
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (/[A-Z]/.test(message[i])) {
+        let passwordNumber = (message[i].charCodeAt() + (key[j % key.length]).charCodeAt() - 130)  % 26;
+        let passwordLetter = ALPHABET.charAt(passwordNumber);
+        passwordString += passwordLetter;
+        j++;
+      } else {
+        passwordString += message[i];
+      }
+    }
+    return this.type === 'direct' ? passwordString : passwordString.split().reverse().join();
+  }
+
   decrypt(message, key) {
-  if (!message, !key) throw "Error"
+    if (message === undefined || key === undefined) throw new "Error";
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let string = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (/[A-Z]/.test(message[i])) {
+        let stringNumber = (message[i].charCodeAt() - (key[j % key.length]).charCodeAt() + 104) % 26;
+        let stringLetter = ALPHABET.charAt(stringNumber);
+        string += stringLetter;
+        j++;
+      } else {
+        string += message[i];
+      }
+    }
+    return this.type === 'direct' ? string : string.split().reverse().join();
   }
 }
 
